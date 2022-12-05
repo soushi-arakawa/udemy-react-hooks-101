114149b6d138557f0b8f2d1e9f41259baacc33f6
Soushi Arakawa <soushi@ageet.com>
2022-12-06 06:11:57 +0900

    create reducer for events

---
+++ src/reducers/index.js
@@ -0,0 +1,42 @@
+// action = {
+//     type: 'CREATE_EVENT',
+//     title: '2022東京オリンピックのお知らせ',
+//     body: '2020年に東京でオリンピックを開催します！つきましては、、、'
+// }
+//
+// # before
+// state = [
+//   { id: 1, title: 'タイトル1', body: 'ボディー1'},  
+//   { id: 2, title: 'タイトル2', body: 'ボディー2'}.
+//   { id: 3, title: 'タイトル3', body: 'ボディー3'},
+// ]
+// 
+// # after
+// state = [
+//   { id: 1, title: 'タイトル1', body: 'ボディー1'},  
+//   { id: 2, title: 'タイトル2', body: 'ボディー2'}.
+//   { id: 3, title: 'タイトル3', body: 'ボディー3'},
+//   {
+//     id: 4,
+//     title: '2022東京オリンピックのお知らせ',
+//     body: '2020年に東京でオリンピックを開催します！つきましては、、、'
+//    } 
+// ]
+
+const events = (state = [], action) => {
+    switch(action.type) {
+        case 'CREATE_EVENT':
+        const event = { title: action.title, body: action.body }
+        let length = state.length
+        let id = length === 0 ? 1 : state[length-1].id + 1
+        return [...state, { id, ...event }]
+        case 'DELETE_EVENT':
+            return state
+        case 'DELETE_ALL_EVENT':
+            return []
+        default:
+            return state
+    }
+}
+
+export default events
