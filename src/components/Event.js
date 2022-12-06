1b9e31788c88af1d45e73427577f4843d37d4fc7
Soushi Arakawa <soushi@ageet.com>
2022-12-07 01:22:25 +0900

    solve prop drilling issue

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useContext } from "react";
 
-const Event = ({ dispatch, event }) => {
-      const id = event.id
-      const handleClickDeleteButton = () => {
-        const result = window.confirm(`イベント(id=${id})を本当に削除して良いですか？`)
-        dispatch({ type: 'DELETE_EVENT', id })
-      }
-      return (
-        <tr>
-          <td>{id}</td>
-          <td>{event.title}</td>
-          <td>{event.body}</td>
-          <td><button type="button" className="btn btn-danger" onClick={handleClickDeleteButton}>削除</button></td>
-        </tr>
-      )
-}
+import { DELETE_EVENT } from "../actions";
+import AppContext from "../contexts/AppContext";
 
-export default Event
+const Event = ({ event }) => {
+  const { dispatch } = useContext(AppContext);
+  const id = event.id;
+  const handleClickDeleteButton = () => {
+    const result = window.confirm(
+      `イベント(id=${id})を本当に削除して良いですか？`
+    );
+    dispatch({ type: DELETE_EVENT, id });
+  };
+  return (
+    <tr>
+      <td>{id}</td>
+      <td>{event.title}</td>
+      <td>{event.body}</td>
+      <td>
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={handleClickDeleteButton}
+        >
+          削除
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default Event;
--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,10 +1,12 @@
-import React, { useReducer, useState } from 'react'
+import React, { useContext, useState } from 'react'
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { CREATE_EVENT, DELETE_ALL_EVENTS } from '../actions'
+import AppContext from '../contexts/AppContext'
 
-const EventForm = ({ state, dispatch }) => {
+const EventForm = () => {
+    const { state, dispatch } = useContext(AppContext)
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
 
